7f25ba2a2fc47ec1ed89bd5d88e804963c46b52b
mailofdev <mailofdeveloper@gmail.com>
2025-10-20 12:37:53 +0530

    minor changes

--- src/components/topbar/BrandLogo.js
+++ src/components/topbar/BrandLogo.js
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
-import logo from '../../assets/images/logo3.jpeg';  
-const BrandLogo = () => (
-  <Link to="/dashboard" className="navbar-brand fw-bold d-flex align-items-center">
-    <img 
-      src={logo} 
-      alt="Level Grit Logo" 
-      style={{ height: "40px",width: "50px" }} 
+import logo from '../../assets/images/logo3.jpeg'; 
+import { getRoutes } from "../navigation/Routes"; 
+const BrandLogo = () => {
+  const routes = getRoutes(); 
+  return (
+    <Link to={routes?.[0]?.href} className="navbar-brand fw-bold d-flex align-items-center">
+      <img 
+        src={logo} 
+        alt="Level Grit Logo" 
+        style={{ height: "40px",width: "50px" }} 
     />
     {/* Level Grit */}
   </Link>
-);
-
+)
+}
 export default BrandLogo;
