afb41449d24510f235b03bfd13a6fef99f3102cc
mailofdev <mailofdeveloper@gmail.com>
2025-11-01 11:58:28 +0530

    updated appbaseurl for HTTP, same local network

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -3,8 +3,13 @@ import axios from "axios";
 import { decryptToken } from "../utils/crypto";
 
 const axiosInstance = axios.create({
-  baseURL: "https://localhost:7240",
-  // baseURL: "https://1a81c84fcb58.ngrok-free.app",
+  //  baseURL: "https://localhost:7240",
+  // If your backend runs on HTTPS (with valid local certs):
+  // baseURL: "https://192.168.31.231:7240",
+
+  // Most common case (HTTP, same local network)
+  baseURL: "http://192.168.31.231:7240",
+ // 👈 include correct port here
   timeout: 10000,
 });
 
